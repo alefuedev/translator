ac35dda8b90c1e8316e209062959fa28a576b1e8
alefuedev <42624248+alefuedev@users.noreply.github.com>
2020-09-26 23:23:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from translate import Translator
 
-location = 'whrite here the location of the file'
+location = 'write here the location of the file'
 
 with  open(location)as poem:
     translator = Translator(to_lang='ja')
